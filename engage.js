5f6db4db22064fcb20e7e35d5909a9b00b152166
DeaconRodda <deacon@sqglz.com>
2020-01-01 18:28:58 -0700

    dot may now be summoned and bannished

--- engage.js
+++ engage.js
@@ -8,6 +8,29 @@
 const makeDot = function() {
 
 	let dot = document.createElement("div");
+	dot.setAttribute("id","dot-1");
 	dot.classList.add("dot");
 	heroBox.appendChild(dot);
 }
+
+const summonDot = function() {
+	makeDot();
+}
+
+const bannishDot = function() {
+	let dot = document.querySelector("#dot-1");
+	heroBox.removeChild(dot);
+}
+
+// Make a dot    ✓
+// Make it appear and disappear
+// Create Lifecycle
+// Make it move toward the edge of the screen
+// Make multiple dots
+// Randomly assign direction
+// Create dot count variable control
+// Create speed variable control
+// Create frequency variable control
+// Make question array
+// Randomly grab questions
+// Polish the type
