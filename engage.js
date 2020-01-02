49b0d8f75a494a502fbd632fb1b20c67c428c1db
DeaconRodda <deacon@sqglz.com>
2020-01-01 18:03:48 -0700

    dot instatiator added

---
+++ engage.js
@@ -0,0 +1,13 @@
+
+// ========= JUNK 
+	const sayHi = function(name) {
+		console.log("Hi, " + name + "!");
+	};
+
+
+const makeDot = function() {
+
+	let dot = document.createElement("div");
+	dot.classList.add("dot");
+	heroBox.appendChild(dot);
+}
